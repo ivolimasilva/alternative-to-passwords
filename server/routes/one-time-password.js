1e0d66dc02ec4a721458be0e4db453b25f503af4
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-04-21 15:49:40 +0100

    OTP updated to only one form factor.

--- server/routes/one-time-password.js
+++ server/routes/one-time-password.js
@@ -22,20 +22,17 @@ module.exports = function (server) {
 			// Validate payload params before handler gets the load
 			validate: {
 				payload: {
-					email: Joi.string().email().required(),
-					password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
+					email: Joi.string().email().required()
 				}
 			},
 			handler: function (request, reply) {
-				Auth.search(request.payload)
-					.then(function () {
-						return reply({ statusCode: 200 });
-
-						// TODO: Create a random code and send it to the mobile app
-					})
-					.catch(function () {
-						return reply(Boom.badData('Incorrect login information.'));
-					});
+				if (request.payload.email == Config.test.email) {
+					// Generate Code
+					// Send code to mobile app
+					return reply({ statusCode: 200 });
+				} else {
+					return reply(Boom.badData('Incorrect login information.'));
+				}
 			}
 		}
 	});
