10274bf6751cfca5023cd98b50c03e12770fa7fa
José Peixoto <ei12134@fe.up.pt>
2017-05-05 14:19:25 +0100

    cant import u2f-api.js

--- server/routes/universal-2nd-factor.js
+++ server/routes/universal-2nd-factor.js
@@ -12,6 +12,57 @@ module.exports = function (server) {
 
 	/*
      * Route for the U2F device registration
+     * Method:  GET
+     * Params:  email		- Email of the user
+     *          password	- Password of the user
+     * Returns: 
+     */
+	server.route({
+		path: '/u2f/register',
+		method: 'GET',
+		config: {
+			// Validate payload params before handler gets the load
+			// validate: {
+			// 	payload: {
+			// 		// email: Joi.string().email().required()
+			// 	}
+			// },
+			handler: function (request, reply) {
+
+				var tokens = [];
+				var appId = 'https://localhost:' + 9000;
+
+				u2f.startRegistration(appId, tokens)
+					.then(function (registrationRequest) {
+						// Save registration request to session for later use
+						// request.session.registrationRequest = registrationRequest;
+
+						console.log(registrationRequest.appId);
+						console.log(registrationRequest.registerRequests);
+						console.log(registrationRequest.registeredKeys);
+
+						// Send registration request to client
+						//reply.json(registrationRequest);
+						return reply({ appId: registrationRequest.appId, registerRequests: registrationRequest.registerRequests, registeredKeys: registrationRequest.registeredKeys });
+
+					}, function (error) {
+						// Handle registration request error
+						// reply.status(500).json(error);
+						return reply({ statusCode: 500, error });
+					});
+
+				// if (request.payload.email == Config.test.email) {
+				// Set flag that server is expecting to receive U2F in meantime
+				// return reply({ statusCode: 200 });
+				// } else {
+				// return reply(Boom.badData('Incorrect login information.'));
+				// }
+			}
+		}
+	});
+
+	/*
+     * Route for the U2F device registration
      * Method:  POST
      * Params:  email		- Email of the user
      *          password	- Password of the user
@@ -29,10 +80,10 @@ module.exports = function (server) {
 			// },
 			handler: function (request, reply) {
 				// if (request.payload.email == Config.test.email) {
-					// Set flag that server is expecting to receive U2F in meantime
-					 return reply({ statusCode: 200 });
+				// Set flag that server is expecting to receive U2F in meantime
+				return reply({ statusCode: 200 });
 				// } else {
-					// return reply(Boom.badData('Incorrect login information.'));
+				// return reply(Boom.badData('Incorrect login information.'));
 				// }
 			}
 		}
@@ -57,8 +108,8 @@ module.exports = function (server) {
 			// },
 			handler: function (request, reply) {
 				// if (request.payload.email == Config.test.email) {
-					// Set flag that server is expecting to receive U2F in meantime
-					return reply({ statusCode: 200 });
+				// Set flag that server is expecting to receive U2F in meantime
+				return reply({ statusCode: 200 });
 				// } else {
 				// 	return reply(Boom.badData('Incorrect login information.'));
 				// }
